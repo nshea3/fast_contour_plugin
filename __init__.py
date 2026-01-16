2b5b6e727dae907da92b8408f6aeb1ba539dfd20
nshea3 <nshea@email.arizona.edu>
2026-01-16 10:42:38 -0700

    moving stuff around

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Fast Contour QGIS Plugin
-High-performance contour generation using contourpy
-"""
-
-def classFactory(iface):
-    from .fast_contour_plugin import FastContourPlugin
-    return FastContourPlugin(iface)
